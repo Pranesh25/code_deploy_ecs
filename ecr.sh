b14555c580765779384af8b53fd9403ad498ed4e
Pranesh25 <41353968+Pranesh25@users.noreply.github.com>
2022-05-16 15:59:12 +0530

    Update ecr.sh

--- ecr.sh
+++ ecr.sh
@@ -1,4 +1,5 @@
-#aws ecr create-repository --repository-name mydocker-test
+#!/bin/bash
+aws ecr create-repository --repository-name mydocker-test
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 090887888547.dkr.ecr.us-east-1.amazonaws.com
 docker build -t mydocker-test .
 docker tag mydocker-test:latest 090887888547.dkr.ecr.us-east-1.amazonaws.com/mydocker-test:latest
